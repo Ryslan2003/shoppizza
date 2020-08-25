488353a19978b26f361aad37800befa3c6edd96d
ryslan <ruslan.ukhlin@mail.ru>
2020-08-25 22:47:13 +0300

    start project

---
+++ resources/js/app.js
@@ -0,0 +1,11 @@
+require('./bootstrap');
+
+window.Vue = require('vue');
+import router from "./router";
+
+
+Vue.component('example-component', require('./components/ExampleComponent.vue').default);
+
+const app = new Vue({
+    router
+}).$mount('#app');
